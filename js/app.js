74673150a1ec186841c6521f7c939c01a2373570
Theo Ventura <theo.ventura1107@gmail.com>
2025-10-27 01:11:29 -0300

    Adicionando contador de jogos alugados no console e alerta para devolução de jogos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
+let jogosAlugados = 0;
+
+function contarJogosAlugados() {
+    console.log(`Número de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
+        alert('AVISO: você está prestes a devolver um jogo, clique OK para confirmar.');
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.innerHTML = 'Alugar';
+        jogosAlugados--;
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.innerHTML = 'Devolver';
+        jogosAlugados++
     }
+
+    contarJogosAlugados();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarJogosAlugados();
+});
